8b464c50d7a38fd11174e493f02e8caeca939d84
Ryan <95035038+ryanv404@users.noreply.github.com>
2022-01-18 23:23:50 -0600

    added various models & routes

--- models/Review.js
+++ models/Review.js
@@ -1,30 +1,75 @@
 const mongoose = require("mongoose");
 
-// Define the schema
-const reviewSchema = new mongoose.Schema({
-  reviewTitle: {
-    type: String,
-    maxlength: [100, "Title is too long (max 100 characters)."],
-    required: [true, "Title cannot be empty."],
-    unique: true
+const ReviewSchema = mongoose.Schema(
+  {
+    rating: {
+      type: Number,
+      min: 1,
+      max: 5,
+      required: [true, "Please provide a rating."],
+    },
+    title: {
+      type: String,
+      trim: true,
+      required: [true, "Please provide a review title."],
+      maxlength: 100,
+    },
+      movieTitle: {
+      type: String,
+      trim: true,
+      required: [true, "Please provide a movie title."],
+      maxlength: 100,
+    },
+    comment: {
+      type: String,
+      required: [true, "Please provide a review text."],
+    },
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    product: {
+      type: mongoose.Schema.ObjectId,
+      ref: "Product",
+      required: true,
+    },
   },
-  movieName: {
-    type: String,
-    maxlength: [100, "Movie name is too long (max 200 characters)."],
-    required: [true, "Movie name cannot be empty."],
-  },
-  reviewBody: {
-    type: String,
-    maxlength: [1000, "Review body is too long (max 1000 characters)."],
-    required: [true, "Review body cannot be empty."],
-  },
-  reviewRating: {
-    type: Number,
-    default: 0,
-  },
-}, {timestamps: true});
+  {timestamps: true}
+);
+ReviewSchema.index({product: 1, user: 1}, {unique: true});
 
-// Define mongoose model
-const Review = mongoose.model("Review", reviewSchema);
+ReviewSchema.statics.calculateAverageRating = async function (productId) {
+  const result = await this.aggregate([
+    {$match: {product: productId}},
+    {
+      $group: {
+        _id: null,
+        averageRating: {$avg: "$rating"},
+        numOfReviews: {$sum: 1},
+      },
+    },
+  ]);
 
-module.exports = Review;
+  try {
+    await this.model("Product").findOneAndUpdate(
+      {_id: productId},
+      {
+        averageRating: Math.ceil(result[0]?.averageRating || 0),
+        numOfReviews: result[0]?.numOfReviews || 0,
+      }
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+ReviewSchema.post("save", async function () {
+  await this.constructor.calculateAverageRating(this.product);
+});
+
+ReviewSchema.post("remove", async function () {
+  await this.constructor.calculateAverageRating(this.product);
+});
+
+module.exports = mongoose.model("Review", ReviewSchema);
--- models/User.js
+++ models/User.js
@@ -2,75 +2,57 @@ const mongoose = require("mongoose");
 const validator = require("validator");
 const bcrypt = require("bcrypt");
 
-// Define the user schema
-const userSchema = new mongoose.Schema(
-  {
-    firstName: {
-      type: String,
-      lowercase: true,
-      trim: true,
-      minlength: [2, "First name is too short (min 2 characters)."],
-      maxlength: [25, "First name is too long (max 25 characters)."],
-      required: [true, "First name cannot be empty."],
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please provide name."],
+    minlength: 3,
+    maxlength: 50,
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: [true, "Please provide email."],
+    validate: {
+      validator: validator.isEmail,
+      message: "Please provide valid email.",
     },
-    lastName: {
-      type: String,
-      lowercase: true,
-      trim: true,
-      minlength: [2, "Last name is too short (min 2 characters)."],
-      maxlength: [25, "Last name is too long (max 25 characters)."],
-      required: [true, "Last name cannot be empty."],
-    },
-    email: {
-      type: String,
-      lowercase: true,
-      trim: true,
-      unique: true,
-      required: [true, "Email cannot be empty."],
-      validate: {
-        validator: validator.isEmail,
-        message: "Please provide a valid email.",
-      },
-    },
-    username: {
-      type: String,
-      lowercase: true,
-      trim: true,
-      unique: true,
-      required: [true, "Username cannot be empty."],
-    },
-    password: {
-      type: String,
-      required: [true, "Password cannot be empty."],
-      minlength: 6,
-    },
-    role: {
-      type: String,
-      enum: ["admin", "user"],
-      default: "user",
-    },
-    verificationToken: String,
-    isVerified: {
-      type: Boolean,
-      default: false,
-    },
-    verificationDate: Date,
-    passwordToken: String,
-    passwordTokenExpirationDate: Date,
-  }, {timestamps: true}
-);
+  },
+  password: {
+    type: String,
+    required: [true, "Please provide password."],
+    minlength: 6,
+  },
+  role: {
+    type: String,
+    enum: ["admin", "user"],
+    default: "user",
+  },
+  verificationToken: String,
+  isVerified: {
+    type: Boolean,
+    default: false,
+  },
+  verified: Date,
+  passwordToken: {
+    type: String,
+  },
+  passwordTokenExpirationDate: {
+    type: Date,
+  },
+}, {timestamps: true});
 
-// Hash password prior to save
-userSchema.pre("save", async function () {
+UserSchema.pre("save", async function () {
+  // console.log(this.modifiedPaths());
+  // console.log(this.isModified('name'));
   if (!this.isModified("password")) return;
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
 });
 
-// Define password comparison method
-userSchema.methods.comparePassword = async function (candidatePassword) {
-  const isMatch = await bcrypt.compare(candidatePassword, this.password);
+UserSchema.methods.comparePassword = async function (canditatePassword) {
+  const isMatch = await bcrypt.compare(canditatePassword, this.password);
   return isMatch;
 };
 
-module.exports = mongoose.model("User", userSchema);
+module.exports = mongoose.model("User", UserSchema);
