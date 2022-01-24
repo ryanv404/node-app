4b10b0fd4246d1402c0deb4f6646ca724483c497
Ryan <95035038+ryanv404@users.noreply.github.com>
2022-01-24 09:05:36 -0600

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis beatae tempora consequuntur cum ipsum autem alias, facere recusandae, adipisci totam esse enim omnis deserunt! A tempore consequuntur temporibus, odio tempora illo laudantium nemo, dolores commodi sed quas sint repellat adipisci debitis quo in cumque optio delectus illum id fugit facere.</p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
